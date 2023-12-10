73ff1e0f08adbb70130f4a79f7b50098fccd7809
HollywoodOKC <T_Lane11@yahoo.com>
2023-12-10 01:34:19 -0600

    Added read me file.

--- server/mongodb.js
+++ server/mongodb.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectdb = () => {
-    mongoose.connect()
+    mongoose.connect("")
     .then(() => {console.log(`Database Connected, Receiving Data`)})
     .catch(console.error);
 }
