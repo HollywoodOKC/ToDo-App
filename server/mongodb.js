3003707e6ba1115f74dd215b8999031cc5a96e9b
HollywoodOKC <T_Lane11@yahoo.com>
2023-12-16 00:51:19 -0600

    Update

--- server/mongodb.js
+++ server/mongodb.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectdb = () => {
-    mongoose.connect("")
+    mongoose.connect("mongodb+srv://tlane11:Oklahoma198621@todolist0.npxwfph.mongodb.net/?retryWrites=true&w=majority")
     .then(() => {console.log(`Database Connected, Receiving Data`)})
     .catch(console.error);
 }
