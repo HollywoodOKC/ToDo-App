67566756e6835df07b29ef69ea90616f472f0b6e
HollywoodOKC <T_Lane11@yahoo.com>
2024-01-13 02:26:55 -0600

    chore: Updated gitignore

--- server/mongodb.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectdb = () => {
-    mongoose.connect("")
-    .then(() => {console.log(`Database Connected, Receiving Data`)})
-    .catch(console.error);
-}
-module.exports = connectdb;
